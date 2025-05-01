c08728a50a7ebb49535af1aea902432bfc782656
Korlall <52757669+Korlall@users.noreply.github.com>
2025-05-01 15:50:49 -0400

    Added first herb

---
+++ js/markers/herb.js
@@ -0,0 +1,10 @@
+// js/markers/waypoint.js - Update marker definitions
+const WaypointMarkers = [
+  {
+    name: 'Sage', 
+    coords: [1132, 3869], 
+    group: 'herb',
+    layer: 'layer1'  // Specify layer
+  }
+  // Add more markers as needed
+];
