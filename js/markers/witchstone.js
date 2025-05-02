0ac7ab9e34204302b53f4848d278187763a6732a
Christopher Popp <chrispopp@hotmail.com>
2025-05-02 17:25:26 -0500

    Add new witchstone marker definitions

--- js/markers/witchstone.js
+++ js/markers/witchstone.js
@@ -11,6 +11,270 @@ const WitchstoneMarkers = [
     coords: [1250, 5015],
     group: 'witchstone',
     layer: 'layer1'  // Specify layer
-  }
+  },
+  {
+    name: 'Coven', 
+    coords: [1787, 1787],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Wilds', 
+    coords: [1747, 1172],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Tower Room', 
+    coords: [2210, 1898],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Street Level', 
+    coords: [1440, 2324],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'West Wickham Sewers', 
+    coords: [980, 1173],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Ruins of West Wickham', 
+    coords: [1095, 1441],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'The Blood Tree', 
+    coords: [520, 1673],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Wickham Sewers', 
+    coords: [862, 1980],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Wickham Marketplace', 
+    coords: [1097, 2132],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Cistern', 
+    coords: [827, 2863],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Crossroads', 
+    coords: [1095, 2747],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Old Wickham Monastery', 
+    coords: [1057, 3208],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Old Wickham Townsquare', 
+    coords: [1249, 3361],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Outskirts', 
+    coords: [1401, 3171],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Old Wickham Living District', 
+    coords: [1249, 3746],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Feralwood', 
+    coords: [1402, 3706],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Ruins', 
+    coords: [1440, 4244],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Gravseep Swamp', 
+    coords: [1056, 4360],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Hunter\'s Trail', 
+    coords: [1286, 4629],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Old Elevator', 
+    coords: [711, 4398],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Dilapidated Mausoleum', 
+    coords: [827, 4745],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Forgotten Catacombs', 
+    coords: [328, 4783],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Catacomb Depths', 
+    coords: [96, 5280],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Millhouse', 
+    coords: [980, 5281],
+    group: 'witchstone',
+    layer: 'layer2'  // Specify layer
+  },
+  {
+    name: 'Hidden Witch Stone', 
+    coords: [1057, 5626],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Lochmuir Road', 
+    coords: [1671, 4514],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Lochmuir Village', 
+    coords: [1632, 5012],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Secret Loft', 
+    coords: [1940, 4782],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Boathouse', 
+    coords: [1439, 5127],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Braer Island', 
+    coords: [1517, 5856],
+    group: 'witchstone',
+    layer: 'layer2'  // Specify layer
+  },
+  {
+    name: 'Braer Castle Left Wing', 
+    coords: [1518, 6126],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Upper Quarters', 
+    coords: [1710, 6204],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Crumbled Chamber', 
+    coords: [2015, 5973],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Theater', 
+    coords: [1671, 6741],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Rooftop Terrace', 
+    coords: [2015, 7049],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Braer Gardens', 
+    coords: [1440, 7124],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Frostbite Pass', 
+    coords: [1671, 7663],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Traveler Camp', 
+    coords: [1747, 8047],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Drakkeden Citdadel Entrance', 
+    coords: [1863, 8277],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Lighthouse', 
+    coords: [1440, 8545],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Torn Village of Smiles', 
+    coords: [1324, 8586],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Lower Citadel', 
+    coords: [1747, 9162],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Upper Citadel', 
+    coords: [2015, 9122],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
+  {
+    name: 'Icewind Perch', 
+    coords: [2054, 9583],
+    group: 'witchstone',
+    layer: 'layer1'  // Specify layer
+  },
   // Add more markers as needed
 ];
