27dff05240468d8d1c006e3119fc7a51c5929776
Korlall <52757669+Korlall@users.noreply.github.com>
2025-05-02 16:17:05 -0400

    Update ore.js

--- js/markers/ore.js
+++ js/markers/ore.js
@@ -12,5 +12,11 @@ const OreMarkers = [
     group: 'ore',
     layer: 'layer1'  // Specify layer
   },
+  {
+    name: 'Copper', 
+    coords: [981, 3592], 
+    group: 'ore',
+    layer: 'layer1'  // Specify layer
+  },
   // Add more markers as needed
 ];
