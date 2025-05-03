a92f6e1badcb5facf284834b7a173c1cf36e7be5
Korlall <52757669+Korlall@users.noreply.github.com>
2025-05-03 19:02:59 -0400

    Update lockpick.js

--- js/markers/lockpick.js
+++ js/markers/lockpick.js
@@ -5,6 +5,12 @@ export const LockpickMarkers = [
     coords: [1633, 6279], 
     group: 'lockpick',
     layer: 'layer1'  // Specify layer
-  }
+  },
+  {
+    name: 'Lockpick', 
+    coords: [1593, 7049], 
+    group: 'lockpick',
+    layer: 'layer1'  // Specify layer
+  },
   // Add more markers as needed
 ];
