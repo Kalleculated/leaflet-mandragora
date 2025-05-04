13870caf0cd77a4d6f52d9ab0e923260671ba26f
Korlall <52757669+Korlall@users.noreply.github.com>
2025-05-04 09:13:54 -0400

    Update diagram.js

--- js/markers/diagram.js
+++ js/markers/diagram.js
@@ -1,6 +1,12 @@
 // js/markers/waypoint.js - Update marker definitions
 export const DiagramMarkers = [
   {
+    name: "Warmage Armor Set", 
+    coords: [673, 1135], 
+    group: 'diagram',
+    layer: 'layer1'  // Specify layer
+  },
+  {
     name: "Astral Runestone", 
     coords: [827, 862], 
     group: 'diagram',
