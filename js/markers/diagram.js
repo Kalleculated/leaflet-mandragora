64a7f19bb506a0ca523ed6b8e2591050a4d75756
Korlall <52757669+Korlall@users.noreply.github.com>
2025-05-04 07:50:52 -0400

    Update diagram.js

--- js/markers/diagram.js
+++ js/markers/diagram.js
@@ -7,6 +7,12 @@ export const DiagramMarkers = [
     layer: 'layer1'  // Specify layer
   },
   {
+    name: 'Nevermore', 
+    coords: [863, 4051], 
+    group: 'diagram',
+    layer: 'layer1'  // Specify layer
+  },
+  {
     name: 'Runestone of Brutality', 
     coords: [1291, 3976], 
     group: 'diagram',
