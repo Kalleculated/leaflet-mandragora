1962fe034a2d82f27914ebc2f5f020f7c1064027
Christopher Popp <chrispopp@hotmail.com>
2025-05-04 14:28:52 -0500

    Enhance item abilities and add new item classes and requirements in item-data.js and vendor.js

--- js/constants/item-data.js
+++ js/constants/item-data.js
@@ -3,21 +3,80 @@ const ItemData = (function() {
     const ABILITIES = {
       RESONANCE: {
         name: "Resonance",
-        description: "Increases damage by 10% for each enemy hit, up to 50%."
+        description: `Spending Agrenaline charges your equipped Two-Handed Weapon.  When fully charged, you can activate 
+          Resonance to empower the weapon, causing all Weapon Attacks to emit damaging shockwaves around their point of impact.
+          The damage of the shockwaves is based on your Weapon Damage.`
       },
       BLADE_DANCE: {
         name: "Blade Dance",
-        description: "Increases damage by 10% for each enemy hit, up to 50%."
-      }
+        description: `Spending Combo Points charges your equipped Daggers.  When fully charged, you can activate Blade Dance to briefly
+          become incorporeal and unleash a series of rapid cuts, distributed randomly between nearby enemies.  Each cut deals damage based
+          on your Weapon Damage.`
+      },
+      INNER_STRENGTH: {
+        name: "Inner Strength",
+        description: `Spending Adrenaline charges your equipped Tower Shield.  When fully charged, you can activate Inner Strength
+          to release a shockwave forward, damaging all enemies in its path.  The damage of the shockwave is based on your Shield Damage`
+      },
+      PARRY: {
+        name: "Parry",
+        description: `Most melee attacks and Physical projectiles can be Parried with Light Shields, if timed correctly.  Successfully Parrying
+          a melee attack damages and Staggers the attacker.  Parried projectiles are deflected instead.`
+      },
+      DISCHARGE: {
+        name: "Discharge",
+        description: `Spending Mana charges your equipped Relic.  When fully charged, you can activate Discharge to release a blast around you,
+        Staggering nearby enemies and applying a Status Effect appropriate to your Relic's element.`
+      },
     };
     
     const PASSIVES = {
       ROOT_CHANCE: (value) => `+${value}% Root Chance`,
-      POISON_CHANCE: (value) => `+${value}% Poison Chance` 
+      POISON_CHANCE: (value) => `+${value}% Poison Chance`,
+      SMASH_CHANCE: (value) => `+${value}% Smash Chance`,
+      MANA: (value) => `+${value} Mana`,
+      BURN_CHANCE: (value) => `+${value}% Burn Chance`,
+      LIGHTBRAND_CHANCE: (value) => `+${value}% Lightbrand Chance`,
+      HEALING_EFFECTIVENESS: (value) => `+${value}% Healing Effectiveness`,
+      MAGIC_CRIT_CHANCE: (value) => `+${value}% Magic Critical Chance`,
+      WEAKNESS_CHANCE: (value) => `+${value}% Weakness Chance`,
+    };
+
+    const REQUIREMENTS = {
+      COMBAT_MASTERY: `Combat Mastery`,
+      FIRE_MASTERY: `Fire Mastery`,
+      WYLD_MASTERY: `Wyld Mastery`,
+      CHAOS_MASTERY: `Chaos Mastery`,
+      EQUIP_LOAD: (value) => `Less than ${value}% Equip Load`,
+      SPIRIT: (value) => `${value} Spirit`,
+    };
+
+    const ITEM_TYPES = {
+      WEAPON: 'weapon',
+      RELIC: 'relic',
+      OFFHAND: 'offhand',
+      ARMOR: 'armor',
+    };
+
+    const ITEM_CLASSES = {
+      GREAT_HAMMER: 'Greathammer',
+      GREAT_SWORD: 'Greatsword',
+      DAGGERS: 'Daggers',
+      ONE_HANDED_MACE: 'One-Handed Mace',
+      ONE_HANDED_SWORD: 'One-Handed Sword',
+      PLATE: 'Plate',
+      LEATHER: 'Leather',
+      CLOTH: 'Cloth',
+      TOWER_SHIELD: 'Tower Shield',
+      FIRE: 'Fire',
+      WYLD: 'Wyld',
     };
   
     return {
       ABILITIES,
-      PASSIVES
+      PASSIVES,
+      REQUIREMENTS,
+      ITEM_TYPES,
+      ITEM_CLASSES,
     };
   })();
--- js/markers/vendor.js
+++ js/markers/vendor.js
@@ -1,4 +1,10 @@
 // js/markers/vendor.js
+const { POISON_CHANCE, ROOT_CHANCE, SMASH_CHANCE, MANA, BURN_CHANCE, LIGHTBRAND_CHANCE, HEALING_EFFECTIVENESS, MAGIC_CRIT_CHANCE, WEAKNESS_CHANCE } = ItemData.PASSIVES;
+const { EQUIP_LOAD, COMBAT_MASTERY, FIRE_MASTERY, WYLD_MASTERY, SPIRIT } = ItemData.REQUIREMENTS;
+const { RESONANCE, DISCHARGE, BLADE_DANCE, INNER_STRENGTH } = ItemData.ABILITIES;
+const { WEAPON, RELIC, OFFHAND, ARMOR } = ItemData.ITEM_TYPES;
+const { GREAT_HAMMER, GREAT_SWORD, DAGGERS, ONE_HANDED_MACE, ONE_HANDED_SWORD, TOWER_SHIELD, PLATE, LEATHER, CLOTH, FIRE, WYLD } = ItemData.ITEM_CLASSES;
+
 const VendorMarkers = [
   {
     name: 'Deeydre and Aille', 
@@ -8,8 +14,9 @@ const VendorMarkers = [
     items: [
       {
         name: 'Earthshaper',
-        type: 'weapon',
-        image: 'assets/items/iron_sword.jpg',
+        type: WEAPON,
+        class: GREAT_HAMMER,
+        image: 'assets/items/earthshaper.jpg',
         price: 11900,
         stats: {
           damage: {
@@ -21,22 +28,23 @@ const VendorMarkers = [
             dexterity: "B",
             spirit: "S"
           },
-          ability: ItemData.ABILITIES.RESONANCE.name,
-          abilityDescription: ItemData.ABILITIES.RESONANCE.description,
+          ability: RESONANCE.name,
+          abilityDescription: RESONANCE.description,
           weight: 17,
           speed: 0.75,
           stagger: 200,
           stamina: 2.8,
-          requires: "Less than 125% equip load",
+          requires: EQUIP_LOAD(125),
           passives: [
-            ItemData.PASSIVES.ROOT_CHANCE(13)
+            ROOT_CHANCE(13)
           ]
         }
       },
       {
         name: 'Serpent Tongue',
-        type: 'weapon',
-        image: 'assets/items/leather_armor.jpg',
+        type: WEAPON,
+        class: GREAT_SWORD,
+        image: 'assets/items/serpent_tongue.jpg',
         price: 10800,
         stats: {
           speed: 1.13,
@@ -46,24 +54,25 @@ const VendorMarkers = [
           damage: {
             physical: 175,
             wyld: 80,
-            light: 50
+            astral: 50
           },
           scaling: {
             strength: "B",
             dexterity: "S",
             spirit: "S+"
           },
-          ability: ItemData.ABILITIES.RESONANCE.name,
-          abilityDescription: ItemData.ABILITIES.RESONANCE.description,
+          ability: RESONANCE.name,
+          abilityDescription: RESONANCE.description,
           passives: [
-            ItemData.PASSIVES.POISON_CHANCE(13)
+            POISON_CHANCE(13)
           ]
         }
       },
       {
         name: 'Dragonfly',
-        type: 'weapon',
-        image: 'assets/items/leather_armor.jpg',
+        type: WEAPON,
+        class: DAGGERS,
+        image: 'assets/items/dragonfly.jpg',
         price: 6400,
         stats: {
           speed: 3.41,
@@ -78,14 +87,339 @@ const VendorMarkers = [
             dexterity: "A+",
             spirit: "A+"
           },
-          ability: ItemData.ABILITIES.BLADE_DANCE.name,
-          abilityDescription: ItemData.ABILITIES.BLADE_DANCE.description,
+          requires: [
+            SPIRIT(15)
+          ],
+          ability: BLADE_DANCE.name,
+          abilityDescription: BLADE_DANCE.description,
           passives: [
-            ItemData.PASSIVES.ROOT_CHANCE(8),
-            ItemData.PASSIVES.POISON_CHANCE(10)
+            ROOT_CHANCE(8),
+            POISON_CHANCE(10)
           ]
         }
-      }
+      },
+      {
+        name: 'Emerald Dream',
+        type: WEAPON,
+        class: ONE_HANDED_MACE,
+        image: 'assets/items/emerald_dream.jpg',
+        price: 8300,
+        stats: {
+          speed: 1.66,
+          weight: 5,
+          stagger: 117,
+          stamina: 1.95,
+          damage: {
+            physical: 45,
+            wyld: 170
+          },
+          scaling: {
+            dexterity: "B+",
+            spirit: "A+"
+          },
+          requires: [
+            SPIRIT(15)
+          ],
+          passives: [
+            ROOT_CHANCE(10),
+            SMASH_CHANCE(35)
+          ]
+        }
+      },
+      {
+        name: 'Witchclaw',
+        type: WEAPON,
+        class: ONE_HANDED_SWORD,
+        image: 'assets/items/witchclaw.jpg',
+        price: 7900,
+        stats: {
+          speed: 2.03,
+          weight: 4.2,
+          stagger: 80,
+          stamina: 1.9,
+          damage: {
+            physical: 25,
+            wyld: 85,
+            chaos: 85
+          },
+          scaling: {
+            dexterity: "D+",
+            spirit: "B",
+            power: "B"
+          },
+          requires: [
+            SPIRIT(10)
+          ],
+          passives: [
+            POISON_CHANCE(10)
+          ]
+        }
+      },
+      {
+        name: "High Priest Breastplate",
+        type: ARMOR,
+        class: PLATE,
+        image: 'assets/items/high_priest_breastplate.jpg',
+        price: 6500,
+        stats: {
+          armor: 40,
+          spirit: 7,
+          knowledge: 7,
+          constitution: 6,
+          weight: 12,
+          poise: 27
+        }
+      },
+      {
+        name: "Grovetender's Jerkin",
+        type: ARMOR,
+        class: LEATHER,
+        image: 'assets/items/groventenders_jerkin.jpg',
+        price: 5370,
+        stats: {
+          armor: 19,
+          spirit: 7,
+          knowledge: 7,
+          dexterity: 7,
+          weight: 8.7,
+          poise: 14
+        }
+      },
+      {
+        name: "Dhaunae's Coat",
+        type: ARMOR,
+        class: CLOTH,
+        image: 'assets/items/dhaunaes_coat.jpg',
+        price: 4885,
+        stats: {
+          armor: 13,
+          spirit: 6,
+          power: 7,
+          constitution: 6,
+          weight: 4.7,
+          poise: 9.2
+        }
+      },
+      {
+        name: "Oakheart",
+        type: OFFHAND,
+        class: TOWER_SHIELD,
+        image: 'assets/items/oakheart.jpg',
+        price: 6390,
+        stats: {
+          weight: 19.5,
+          poise: 28,
+          balance: 95,
+          armor: 50,
+          damage: {
+            physical: 100,
+            wyld: 105,
+          },
+          scaling: {
+            dexterity: "C+",
+            spirit: "S+",
+            strength: "C+"
+          },
+          requires: [
+            EQUIP_LOAD(125),
+            COMBAT_MASTERY,
+          ],
+          ability: INNER_STRENGTH.name,
+          abilityDescription: INNER_STRENGTH.description,
+          passives: [
+            POISON_CHANCE(10)
+          ],
+          defense: {
+            physical: 100,
+            burn: 85,
+            frost: 85,
+            wyld: 85,
+            chaos: 85,
+            // there's an extra +30% wlyd on this shield that I don't understand
+          }
+        }
+      },
+      {
+        name: "Ashfall",
+        type: RELIC,
+        class: FIRE,
+        image: 'assets/items/ashfall.jpg',
+        price: 4920,
+        stats: {
+          weight: 1.6,
+          spell_buff: 70,
+          requires: [
+            FIRE_MASTERY,
+          ],
+          passives: [
+            MANA(120),
+            BURN_CHANCE(15),
+          ],
+          ability: DISCHARGE.name,
+          abilityDescription: DISCHARGE.description,
+        }
+      },
+      {
+        name: "Earthsong Talisman",
+        type: RELIC,
+        class: WYLD,
+        image: 'assets/items/earthsong_talisman.jpg',
+        price: 4920,
+        stats: {
+          weight: 1.6,
+          spell_buff: 70,
+          requires: [
+            WYLD_MASTERY,
+          ],
+          passives: [
+            MANA(120),
+            ROOT_CHANCE(15),
+          ],
+          ability: DISCHARGE.name,
+          abilityDescription: DISCHARGE.description,
+        }
+      },
+      {
+        name: "High Priest Legplates",
+        type: ARMOR,
+        class: PLATE,
+        image: 'assets/items/high_priest_legplates.jpg',
+        price: 5000,
+        stats: {
+          weight: 7,
+          poise: 16,
+          armor: 17.5,
+          strength: 6,
+          spirit: 6,
+          endurance: 6,
+        }
+      },
+      {
+        name: "Grovetender's Pants",
+        type: ARMOR,
+        class: LEATHER,
+        image: 'assets/items/grovetenders_pants.jpg',
+        price: 4130,
+        stats: {
+          weight: 5.1,
+          poise: 8.5,
+          armor: 19,
+          dexterity: 6,
+          spirit: 6,
+          defense: 6,
+        }
+      },
+      {
+        name: "Dhaunae's Leggings",
+        type: ARMOR,
+        class: CLOTH,
+        image: 'assets/items/dhaunaes_leggings.jpg',
+        price: 3760,
+        stats: {
+          weight: 4,
+          poise: 6.2,
+          armor: 7.7,
+          power: 6,
+          spirit: 6,
+          endurance: 6,
+        }
+      },
+      {
+        name: "High Priest Halo",
+        type: ARMOR,
+        class: PLATE,
+        image: 'assets/items/high_priest_halo.jpg',
+        price: 3850,
+        stats: {
+          weight: 4.8,
+          poise: 9,
+          armor: 9,
+          passives: [
+            LIGHTBRAND_CHANCE(10),
+            HEALING_EFFECTIVENESS(12),
+          ],
+        }
+      },
+      {
+        name: "Grovetender's Crown",
+        type: ARMOR,
+        class: LEATHER,
+        image: 'assets/items/grovetenders_crown.jpg',
+        price: 3200,
+        stats: {
+          weight: 3.4,
+          poise: 5.7,
+          armor: 4.5,
+          passives: [
+            ROOT_CHANCE(10),
+            MAGIC_CRIT_CHANCE(13),
+          ],
+        }
+      },
+      {
+        name: "Dhaunae's Hat",
+        type: ARMOR,
+        class: CLOTH,
+        image: 'assets/items/dhaunaes_hat.jpg',
+        price: 2890,
+        stats: {
+          weight: 2.85,
+          poise: 4,
+          armor: 1,
+          knowledge: 5,
+          passives: [
+            WEAKNESS_CHANCE(10),
+          ],
+        }
+      },
+      {
+        name: "High Priest Bracers",
+        type: ARMOR,
+        class: PLATE,
+        image: 'assets/items/high_priest_bracers.jpg',
+        price: 2960,
+        stats: {
+          weight: 2.5,
+          poise: 6,
+          armor: 4,
+          passives: [
+            LIGHTBRAND_CHANCE(7),
+            HEALING_EFFECTIVENESS(8),
+          ],
+        }
+      },
+      {
+        name: "Grovetender's Gloves",
+        type: ARMOR,
+        class: LEATHER,
+        image: 'assets/items/grovetenders_gloves.jpg',
+        price: 2450,
+        stats: {
+          weight: 1.4,
+          poise: 2.8,
+          armor: 2.3,
+          passives: [
+            MAGIC_CRIT_CHANCE(10),
+            ROOT_CHANCE(7),
+          ],
+        }
+      },
+      {
+        name: "Dhaunae's Gloves",
+        type: ARMOR,
+        class: CLOTH,
+        image: 'assets/items/dhaunaes_gloves.jpg',
+        price: 2225,
+        stats: {
+          weight: 1.1,
+          poise: 1.7,
+          armor: 0.85,
+          knowledge: 5,
+          passives: [
+            WEAKNESS_CHANCE(7),
+          ],
+        }
+      },
     ]
   }
 ];
