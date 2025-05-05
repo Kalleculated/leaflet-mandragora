08f8c56a112d6d67d9f5269930d0f765ccefc73f
Korlall <52757669+Korlall@users.noreply.github.com>
2025-05-05 14:47:04 -0400

    Update cat.js

--- js/markers/cat.js
+++ js/markers/cat.js
@@ -1,52 +1,86 @@
 // js/markers/cat.js - Update marker definitions
 export const CatMarkers = [
-    {
-      name: 'Cat',
-      coords: [1094, 2196],
+        {
+      name: 'Cat Location',
+      coords: [
+        [1094, 2196],
+        [1891, 6425],
+        [1249, 636],
+        [1518, 8125],
+        [1479, 2171],
+        [1748, 8163],
+      ],
       group: 'cat',
       layer: 'layer1',
-      },
-    {
-      name: 'Cat',
-      coords: [1891, 6425],
-      group: 'cat',
-      layer: 'layer1',
-      },
-    {
-      name: 'Cat',
-      coords: [1939, 4832],
+      items: [
+        {
+          name: 'Cat',
+          type: 'cat'
+        },
+      ]
+    },
+        {
+      name: 'Cat Location',
+      coords: [
+        [1939, 4832],
+        [982, 5244],
+      ],
       group: 'cat',
       layer: 'layer2',
-      },
-    {
-      name: 'Cat',
-      coords: [1249, 636],
-      group: 'cat',
-      layer: 'layer1',
-      },
-    {
-      name: 'Cat',
-      coords: [982, 5244],
-      group: 'cat',
-      layer: 'layer2',
-      },
-    {
-      name: 'Cat',
-      coords: [1518, 8125],
-      group: 'cat',
-      layer: 'layer1',
-      },
-    {
-      name: 'Cat',
-      coords: [1479, 2171],
-      group: 'cat',
-      layer: 'layer1',
-      },
-    {
-      name: 'Cat',
-      coords: [1748, 8163],
-      group: 'cat',
-      layer: 'layer1',
-      },  // Specify layer
+      items: [
+        {
+          name: 'Cat',
+          type: 'cat'
+        },
+      ]
+    },
+    // {
+    //   name: 'Cat',
+    //   coords: [1094, 2196],
+    //   group: 'cat',
+    //   layer: 'layer1',
+    //   },
+    // {
+    //   name: 'Cat',
+    //   coords: [1891, 6425],
+    //   group: 'cat',
+    //   layer: 'layer1',
+    //   },
+    // {
+    //   name: 'Cat',
+    //   coords: [1939, 4832],
+    //   group: 'cat',
+    //   layer: 'layer2',
+    //   },
+    // {
+    //   name: 'Cat',
+    //   coords: [1249, 636],
+    //   group: 'cat',
+    //   layer: 'layer1',
+    //   },
+    // {
+    //   name: 'Cat',
+    //   coords: [982, 5244],
+    //   group: 'cat',
+    //   layer: 'layer2',
+    //   },
+    // {
+    //   name: 'Cat',
+    //   coords: [1518, 8125],
+    //   group: 'cat',
+    //   layer: 'layer1',
+    //   },
+    // {
+    //   name: 'Cat',
+    //   coords: [1479, 2171],
+    //   group: 'cat',
+    //   layer: 'layer1',
+    //   },
+    // {
+    //   name: 'Cat',
+    //   coords: [1748, 8163],
+    //   group: 'cat',
+    //   layer: 'layer1',
+    //   },  // Specify layer
     // Add more markers as needed
   ];
