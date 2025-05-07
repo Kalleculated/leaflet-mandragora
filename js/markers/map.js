0c4a99cf1997b42fc32f74ac39ce055e71591c80
Korlall <52757669+Korlall@users.noreply.github.com>
2025-05-07 01:13:49 -0400

    Update map.js

--- js/markers/map.js
+++ js/markers/map.js
@@ -1,6 +1,12 @@
 // js/markers/waypoint.js - Update marker definitions
 export const MapMarkers = [
   {
+    name: 'Lochmuir Village', 
+    coords: [1751, 5125], 
+    group: 'map',
+    layer: 'layer1'  // Specify layer
+  },
+  {
     name: 'Forgotten Catacombs', 
     coords: [406, 5048], 
     group: 'map',
