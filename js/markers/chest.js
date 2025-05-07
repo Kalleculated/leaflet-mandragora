7c3fb912a0dab57c1c0075239d8acbcef5156618
Korlall <52757669+Korlall@users.noreply.github.com>
2025-05-06 23:07:04 -0400

    Update chest.js

--- js/markers/chest.js
+++ js/markers/chest.js
@@ -1,6 +1,18 @@
 // js/markers/chest.js - Chest markers with item contents
 export const ChestMarkers = [
     {
+      name: 'Silver Chest', //There are wood, bronze, silver and gold chests
+      coords: [1287, 1287], 
+      group: 'chest',
+      layer: 'layer1',
+      items: [
+        {
+          name: 'Scarlet Militia Set',
+          type: 'diagram',
+        },
+      ]
+    },
+    {
       name: 'Gold Chest', //There are wood, bronze, silver and gold chests
       coords: [786, 2092], 
       group: 'chest',
