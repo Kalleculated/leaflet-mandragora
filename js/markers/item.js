1036979de7a0e33ec3b2f0a09aa89b4e6d4c4fd7
Korlall <52757669+Korlall@users.noreply.github.com>
2025-05-06 23:14:07 -0400

    Update item.js

--- js/markers/item.js
+++ js/markers/item.js
@@ -48,6 +48,20 @@ export const ItemMarkers = [
     {
       name: 'Item Location',
       coords: [
+        [1212, 1555],
+      ],
+      group: 'item',
+      layer: 'layer1',
+      items: [
+        {
+          name: 'Royal Scepter',
+          type: 'diagram'
+        },
+      ]
+    },
+    {
+      name: 'Item Location',
+      coords: [
         [1056, 1325],
       ],
       group: 'item',
