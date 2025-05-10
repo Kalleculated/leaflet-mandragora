4e3505e37d410d74f15994bf2ec29f5c3bde2df5
Korlall <52757669+Korlall@users.noreply.github.com>
2025-05-10 07:36:55 -0400

    Update item.js

--- js/markers/item.js
+++ js/markers/item.js
@@ -4,6 +4,7 @@ export const ItemMarkers = [
       name: 'Lockpick Location',
       coords: [
         [1670, 7013],
+        [635, 1172],
         [1671, 7277],
         [446, 981],
         [1786, 6240],
